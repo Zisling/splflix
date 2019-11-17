b141f63135b3b4eca5eff164ff37ee0bd4315fca
zisling <hedi120@gmail.com>
2019-11-18 01:09:08 +0200

    move to folders

--- test.cpp
+++ test.cpp
@@ -1,9 +1,34 @@
+#include "json.hpp"
 #include <iostream>
-#include <include/json.hpp>
 #include <fstream>
-using namespace std;
-int main() {
-    std::ifstream i("config1.json");
 
+using json = nlohmann::json;
+template<class UnaryFunction>
+void recursive_iterate(const json& j, UnaryFunction f) {
+    for (auto it = j.begin(); it != j.end(); ++it) {
+        if (it->is_structured()) {
+            recursive_iterate(*it, f);
+        } else {
+            f(it);
+        }
+    }
+}
+int main() {
+    std::ifstream f("/home/zisling/CLionProjects/splflix/config1.json");
+    json j;
+    f>>j;
+//    recursive_iterate(j,[](json::const_iterator it){
+//        std::cout <<*it << std::endl;
+//
+//    });
+    std::cout <<j.is_structured() << std::endl;
+    for (const json &item : j) {
+        std::cout << item.is_structured() << std::endl;
+        for (const auto &item2 : item) {
+                std::cout <<item2["name"] << std::endl;
+                std::cout <<item2["tags"] << std::endl;
+        }
+
+    }
 
 }
