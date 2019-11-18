2ab3126a28a7d15103e91c0825bf6e5e20714ace
zisling <hedi120@gmail.com>
2019-11-18 15:35:43 +0200

    JsonReader is finish but not clean

---
+++ JsonReader.cpp
@@ -0,0 +1,98 @@
+//
+// Created by zisling on 18/11/2019.
+//
+
+#include "JsonReader.h"
+
+#include <utility>
+
+JsonReader::JsonReader(const std::string& path):tags_(new std::vector<std::string>),seasons_(new std::vector<int>) {
+    std::ifstream f(path);
+    f>>j_;
+    type_iterator_ = j_.begin();
+    i_=type_iterator_.value();
+    watchable_iterator_= i_.begin();
+    type_=type_iterator_.key();
+    nxet_watchable();
+
+
+}
+
+JsonReader::~JsonReader() {
+    delete seasons_;
+    delete tags_;
+}
+
+
+const json &JsonReader::getJ() const {
+    return j_;
+}
+
+const std::string &JsonReader::getType() const {
+    return type_;
+}
+
+const std::string &JsonReader::getName() const {
+    return name_;
+}
+
+int JsonReader::getLength() const {
+    return length_;
+}
+
+int JsonReader::getEpisodeLength() const {
+    return episode_length_;
+}
+
+const std::vector<int> &JsonReader::getSeasons() const {
+    return *seasons_;
+}
+
+const std::vector<std::string> &JsonReader::getTags() const {
+    return *tags_;
+}
+
+void JsonReader::nxet_watchable() {
+        tags_->clear();
+        seasons_->clear();
+        episode_length_=-1;
+        length_=-1;
+        name_="";
+        type_=type_iterator_.key();
+        if (type_.empty()){empty_=true;}
+        if (!isEmpty()){
+        if(type_=="movies"){
+            name_=(*watchable_iterator_)["name"];
+            length_=(*watchable_iterator_)["length"];
+            for (const auto &item : (*watchable_iterator_)["tags"]) {
+                tags_->push_back(item);
+            }
+        }
+        else if(type_=="tv_series"){
+            name_=(*watchable_iterator_)["name"];
+            episode_length_=(*watchable_iterator_)["episode_length"];
+            for (const auto &item : (*watchable_iterator_)["tags"]) {
+                tags_->push_back(item);
+            }
+            for (const auto &item : (*watchable_iterator_)["seasons"]) {
+                seasons_->push_back(item);
+            }
+        }}
+        push_next();
+
+}
+
+void JsonReader::push_next() {
+    ++watchable_iterator_;
+    if (watchable_iterator_ == i_.end()){
+        ++type_iterator_;
+        if (type_iterator_ != j_.end()) {
+            i_=type_iterator_.value();
+            watchable_iterator_ = i_.begin();
+        }
+    }
+}
+
+bool JsonReader::isEmpty() {
+    return empty_;
+}
---
+++ JsonReader.h
@@ -0,0 +1,56 @@
+//
+// Created by zisling on 18/11/2019.
+//
+
+#ifndef SPLFLIX_JSONREADER_H
+#define SPLFLIX_JSONREADER_H
+
+#include <iostream>
+#include "json.hpp"
+#include <fstream>
+using json = nlohmann::json;
+
+
+class JsonReader {
+public:
+    JsonReader(const std::string& path);
+
+    virtual ~JsonReader();
+
+
+    const json &getJ() const;
+
+    const std::string &getType() const;
+
+    const std::string &getName() const;
+
+    int getLength() const;
+
+    int getEpisodeLength() const;
+
+    const std::vector<int> &getSeasons() const;
+
+    const std::vector<std::string> &getTags() const;
+
+    bool isEmpty();
+
+    void nxet_watchable();
+
+
+private:
+    json j_;
+    json::value_type i_;
+    std::string type_;
+    std::string name_;
+    int length_;
+    int episode_length_;
+    bool empty_;
+    std::vector<int > *seasons_;
+    std::vector<std::string> *tags_;
+    json::iterator type_iterator_;
+    json::iterator watchable_iterator_;
+    void push_next();
+};
+
+
+#endif //SPLFLIX_JSONREADER_H
