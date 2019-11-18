57030be98e03754a60a15b673668be87d882fc89
nir <speednir123@gmail.com>
2019-11-18 09:37:07 +0200

    com

--- src/Watchable.cpp
+++ src/Watchable.cpp
@@ -1,4 +1,38 @@
 //
 // Created by zisling on 17/11/2019.
 //
+#include "include/Watchable.h"
 
+Watchable::Watchable(long id, int length, const std::vector<std::string> &tags):id(id),length(length){
+this->tags=tags;
+}
+
+Watchable::~Watchable() {
+tags.clear();
+}
+
+
+Movie::Movie(long id, const std::string &name, int length, const std::vector<std::string> &tags):Watchable(id,length,tags),name(name) {
+
+}
+
+std::string Movie::toString(bool print_full) const {
+    return name;
+}
+
+Watchable *Movie::getNextWatchable(Session &) const {
+    return nullptr;
+}
+
+Episode::Episode(long id, const std::string &seriesName, int length, int season, int episode,
+                 const std::vector<std::string> &tags):Watchable(id,length,tags),seriesName(seriesName),season(season),episode(episode) {
+
+}
+
+std::string Episode::toString(bool print_full) const {
+    return seriesName+" S"+std::to_string(season)+"E"+std::to_string(episode);
+}
+
+Watchable *Episode::getNextWatchable(Session &) const {
+    return nullptr;
+}
