30f48197d417d33add68aa47964252c50022d840
nir <speednir123@gmail.com>
2019-11-21 00:03:33 +0200

    fix User.cpp (cherry picked from commit d859452822bad634f1be222acebdd5ffc5fe3678)

--- src/User.cpp
+++ src/User.cpp
@@ -4,25 +4,81 @@
 #include <iostream>
 #include "algorithm"
 
-/*User abst*/
-User::User(const std::string &name):name(name),history() {
 
+/**
+ *
+ *                    User
+ *
+ */
+
+//User Contructor
+User::User(const std::string &name)
+:name(name),history() {
 }
 
+//User Copy Constructor
+User::User(const User &other)
+:name(other.name) {
+    for (const auto &item : other.history) {
+        this->history.push_back(item);
+    }
+}
+
+//User Move Constructor
+User::User(User &&other)
+:name(other.name) {
+    for (const auto &item : other.history) {
+        this->history.push_back(item);
+    }
+    other.history.clear();
+}
+
+//User Copy Assignment Operator
+User &User::operator=(User &other) {
+    if(this!=&other)
+    {
+        this->history.clear();
+        for (const auto &item : other.history) {
+            this->history.push_back(item);
+        }
+    }
+    return *this;
+}
+//User Move Assignment Operator
+User &User::operator=(User &&other) {
+    if(this!=&other)
+    {
+        this->history.clear();
+        for (const auto &item : other.history) {
+            this->history.push_back(item);
+        }
+        other.history.clear();
+    }
+    return *this;
+}
+
+//User getName
 std::string User::getName() const {
     return name;
 }
-
+//User getHistory
 std::vector<Watchable *> User::get_history() const {
     return history;
 }
-
+//User Destructor
 User::~User() {
     for (auto &item : history) {
         delete item;
     }
 
-}
+
+/***
+ *
+ *                      length_Recommender_User
+ *
+ *
+ *
+ * ***/
 
 /***lengthRecommder***/
 LengthRecommenderUser::LengthRecommenderUser(const std::string &name) : User(name) ,avg(0){
@@ -46,9 +102,13 @@ Watchable *LengthRecommenderUser::getRecommendation(Session &s) {
 
 
 
-/***
- *rerun
- ***/
+
+/**
+ *
+ *              Rerun_Recommender_User
+ *
+ *
+ */
 RerunRecommenderUser::RerunRecommenderUser(const std::string &name) : User(name), Index(0) {
 
 }
@@ -61,9 +121,17 @@ Watchable *RerunRecommenderUser::getRecommendation(Session &s) {
     }
     return toRecommend;
 }
-/***
- *genreRecommender
- ***/
+
+
+
+/**
+ *
+ *              Genre_Recommender_User
+ *
+ *
+ */
+
+
 GenreRecommenderUser::GenreRecommenderUser(const std::string &name) : User(name) {
 
 }
